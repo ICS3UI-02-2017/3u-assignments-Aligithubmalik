daf992388ebd90faf6409be4291450b5fab5f6eb
malia5277 <malia5277@HRH-IC0027321.wrdsb.ca>
2018-02-12 10:38:36 -0500

    Initialize Repository

---
+++ src/Examples/RobotExample.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+import becker.robots.City;
+
+/**
+ * Learning how to use the robots
+ * @author malia5277
+ */
+public class RobotExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+ // Create a City for the Robot   
+ City kw = new City();    
+ 
+ //put a robot in kw
+ 
+ //Sets where marcel starts and is looking 
+       
+    }
+}
